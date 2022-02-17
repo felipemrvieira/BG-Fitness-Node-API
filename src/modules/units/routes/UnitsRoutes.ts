1a3c2b58b27545cd0b65931b2ec90ec1ba888541
felipemrvieira <felipemrvieira@gmail.com>
2022-02-17 14:37:37 -0300

    Create validation middleware

--- src/modules/units/routes/UnitsRoutes.ts
+++ src/modules/units/routes/UnitsRoutes.ts
@@ -1,13 +1,52 @@
 import { Router } from 'express';
 import UnitsController from '../controllers/UnitsController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const unitsRouter = Router();
 const unitsController = new UnitsController();
 
 unitsRouter.get('/', unitsController.index);
-unitsRouter.get('/:id', unitsController.show);
-unitsRouter.post('/', unitsController.create);
-unitsRouter.put('/:id', unitsController.update);
-unitsRouter.delete('/:id', unitsController.delete);
+unitsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  unitsController.show,
+);
+unitsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      address: Joi.string().required(),
+    },
+  }),
+
+  unitsController.create,
+);
+unitsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      address: Joi.string().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  unitsController.update,
+);
+unitsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  unitsController.delete,
+);
 
 export default unitsRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,6 +5,8 @@ import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
+import { celebrate } from 'celebrate';
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
